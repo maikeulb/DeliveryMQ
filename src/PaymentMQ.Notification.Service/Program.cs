457fdd9885d09468d4425d155d4e4c1240f2f7b2
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-18 16:42:47 -0400

    initial commit

---
+++ src/PaymentMQ.Notification.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PaymentMQ.Notification.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
