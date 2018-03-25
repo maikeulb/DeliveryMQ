518c2e51564044f6a6f0c619c150d431e20bd550
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-25 19:15:11 -0400

    use guid

--- src/DeliveryMQ.Api/Models/Register.cs
+++ src/DeliveryMQ.Api/Models/Register.cs
@@ -4,7 +4,7 @@ namespace DeliveryMQ.Api.Models
 {
     public class RegisterModel
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
 		public string Name { get; set; }
 		public string Address { get; set; }
 		public string City { get; set; }
